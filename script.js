0671d549ac66eeb6055736cb7966e9a3d9f6ff73
anshulbaddi <69763264+anshulbaddi@users.noreply.github.com>
2021-07-18 10:12:48 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', onClick, false)
+    function onClick () {
+        var text = document.getElementById('text').value
+        chrome.tabs.query({currentWindow:true, active:true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, text)
+        })
+    }
+}, false)
